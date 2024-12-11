a61498b0d316fb3fb6fd27eab3dd532755d1beb3
monishmuthu22 <monish.muthu22@gmail.com>
2024-12-10 22:50:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,16 +16,20 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selection <= 5)
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selection = 5)
 
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st. subheader(fruit_chosen + ' Nutrition Information' )
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        ingredients_string += fruit_chosen + • •
+       
+        search_on=pd_df.loc[pd_df ['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]        
+        # st.write( 'The search value for', fruit_chosen, ' is ', search_on, *.')
+        
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     #st.write(ingredients_string) 
 
